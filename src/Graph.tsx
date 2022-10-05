f4886ee4625b3acadfda33de08922cbb63b2965b
Paul Koenig <paul.koenig.002@gmail.com>
2022-10-05 13:39:45 -0400

    Add graph to client view whose y-axis is the top_ask_price and x-axis is the stock's timestamp. Remove dumplicated data in graph.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,6 +49,21 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      // Add a y_line graph to table
+      elem.setAttribute('view', 'y_line');
+      // Distinguish stocks ABC and DEF with column-pivots
+      elem.setAttribute('column-pivots', '["stock"]');
+      // Map datapoints based on timestamp on x-axis
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      // Focus on the top_ask_price when plotting graph
+      elem.setAttribute('columns', '["top_ask_price"]');
+      // Handle duplicated data by averaging top_bid_prices and
+      // top_ask_prices of similar data points
+      elem.setAttribute('aggregates', `
+        {"stock":"distinct count",
+        "top_ask_price":"avg",
+        "top_bid_price:":"avg",
+        "timestamp":"distinct count"}`);
     }
   }
 
